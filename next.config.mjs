3125821dc85dc8fa37cffcaf96a2778cd845223f
Roman Khomych <wentris71@gmail.com>
2026-03-19 16:35:10 +0700

    Add CI/CD workflow and fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-import packageJson from './package.json' with { type: 'json' };
-
 const nextConfig = {
-  /* config options here */
-  output: 'export',
-  distDir: 'dist',
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: `/${packageJson.name}`,
 };
 
 export default nextConfig;
