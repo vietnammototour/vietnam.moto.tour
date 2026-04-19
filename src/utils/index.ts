6e0457387a889c8be58d46ada2d60b50260ca18d
Roman Khomych <wentris71@gmail.com>
2026-04-19 22:10:47 +0700

    refactor: remove instagramLink from ContactInfo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,15 @@
-import type { ContactInfo } from "@/types";
+import type {ContactInfo} from '@/types';
 
 export const getUrl = (path: string): string =>
-  `${process.env.__NEXT_ROUTER_BASEPATH || ""}/${path}`;
+  `${process.env.__NEXT_ROUTER_BASEPATH || ''}/${path}`;
 
 export const contactInfo: ContactInfo = {
-  phone: "+84-935-797-550",
-  email: "easyridermotorbiketour@gmail.com",
+  phone: '+84-935-797-550',
+  email: 'easyridermotorbiketour@gmail.com',
   youtubeLink:
-    "https://youtube.com/@vietnammotorcycletour6674?si=kOduPDV6PDhNygvJ",
-  tripadvisorLink: "#",
-  instagramLink: "#",
-  whatsApp: "+84-935-797-550",
-  address: "Alley 05-07 Nguyen Trung Truc st.",
-  city: "Nha Trang City",
+    'https://youtube.com/@vietnammotorcycletour6674?si=kOduPDV6PDhNygvJ',
+  tripadvisorLink: '#',
+  whatsApp: '+84-935-797-550',
+  address: 'Alley 05-07 Nguyen Trung Truc st.',
+  city: 'Nha Trang City',
 };
