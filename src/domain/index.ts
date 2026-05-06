806f844a58398336399fa75025cc344da038fbd0
Roman Khomych <wentris71@gmail.com>
2026-05-06 12:24:13 +0700

    feat(domain): aggregate VMT namespace re-exports

---
+++ src/domain/index.ts
@@ -0,0 +1,8 @@
+export type {LocalizedText} from './shared/localized-text';
+export type {Tour, TourStatus} from './tour';
+export type {ItineraryDay, ItineraryItem} from './tour/itinerary';
+export type {PricingGroup, PricingTier} from './tour/pricing';
+export type {Destination, DestinationWithStats} from './destination';
+export type {Highlight} from './highlight';
+export type {User, Role} from './user';
+export type {Translation} from './translation';
