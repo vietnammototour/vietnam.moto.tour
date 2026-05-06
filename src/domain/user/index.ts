fe1fdd06ad29b1b80486902de736b9597bd2ae79
Roman Khomych <wentris71@gmail.com>
2026-05-06 12:22:51 +0700

    feat(domain): add User entity derived from Prisma

---
+++ src/domain/user/index.ts
@@ -0,0 +1,7 @@
+import type {User as PrismaUser, Role as PrismaRole} from '@prisma/client';
+
+export type Role = PrismaRole;
+
+export type User = Omit<PrismaUser, 'passwordHash' | 'updatedAt'> & {
+  createdAt: string;
+};
